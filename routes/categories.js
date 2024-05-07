93a2d1949003d1652c9c1b2fbe6171091ed4b0d0
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2024-05-07 18:53:30 +0200

    se agrega autenticacion obligatoria para poder usar los controller de categories

--- routes/categories.js
+++ routes/categories.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const CategoryController = require("../controllers/CategoryController");
+const { authentication } = require('../middleware/authentication');
 const router = express.Router();
+
+router.use(authentication);
 
 router.post("/", CategoryController.create);
 router.get("/", CategoryController.getAll);

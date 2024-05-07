4232a331f2035ac80d4990d6d4da8c0268f58183
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2024-05-07 18:47:30 +0200

    correccion de codigo

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -3,15 +3,15 @@ const { Order, Product, ProductOrder } = require("../models/index");
 const OrderController = {
   async createOrder(req, res) {
     try {
-      const { products } = req.body; // Supongamos que los productos a agregar al pedido se reciben como un array de IDs en req.body.products
-      const userId = req.user.id; // Suponiendo que la información del usuario está disponible en req.user después de la autenticación con JWT
+      const { products } = req.body; 
+      const userId = req.user.id; 
 
-      // Crear el pedido
+      
       const newOrder = await Order.create({
         UserId: userId,
       });
 
-      // Agregar los productos al pedido en la tabla ProductOrder
+      
       if (products && products.length > 0) {
         await Promise.all(
           products.map(async (productId) => {
@@ -23,9 +23,9 @@ const OrderController = {
         );
       }
 
-      // Obtener el pedido con los productos asociados cargados
+      
       const orderWithProducts = await Order.findByPk(newOrder.id, {
-        include: [Product], // Esto cargará los productos asociados al pedido
+        include: [Product], 
       });
 
       res.status(201).send({
@@ -41,7 +41,7 @@ const OrderController = {
   async getOrdersWithProducts(req, res) {
     try {
       const ordersWithProducts = await Order.findAll({
-        include: [Product], // Esto cargará los productos asociados a cada pedido
+        include: [Product], 
       });
 
       res.status(200).send(ordersWithProducts);
--- index.js
+++ index.js
@@ -2,22 +2,22 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-// Middleware para parsear el cuerpo de las solicitudes JSON
+
 app.use(express.json());
 
-// Middleware de registro de solicitudes
+
 app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
   next();
 });
 
-// Rutas de la aplicación
+
 app.use("/products", require("./routes/products"));
 app.use("/categories", require("./routes/categories"));
 app.use("/users", require("./routes/users"));
 app.use("/orders", require("./routes/orders"));
 
-// Middleware de manejo de errores
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Error interno del servidor");
